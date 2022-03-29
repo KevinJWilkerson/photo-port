1dcb8338f6ddcfa1e61313790e74513e239b05bb
Kevin <kevin.j.wilkerson@gmail.com>
2022-03-28 20:50:08 -0500

    added tests for Gallery and PhotoList

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,20 +1,19 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-
-import About from "..";
+// __tests__/About.test.js
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
 
 afterEach(cleanup);
 
-describe("About componenet", () => {
-  // First test
-  it("renders", () => {
+describe('About component renders', () => {
+  it('renders', () => {
     render(<About />);
   });
-  // Second test
-  it("matches snapshot DOM node structure", () => {
-    // render About
+  
+  it('matches snapshot DOM node structure', () => {
     const { asFragment } = render(<About />);
+    
     expect(asFragment()).toMatchSnapshot();
   });
-});
+})
